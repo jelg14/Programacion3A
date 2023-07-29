1d7479a8a32f010009e3b248409551f9717bbb0c
José López <jeduardo14lopez@gmail.com>
2023-07-29 15:20:20 -0600

    código actualizado y script de backup

--- Crud2023/Crud2023/Form1.cs
+++ Crud2023/Crud2023/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Crud2023.Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ namespace Crud2023
 {
     public partial class Form1 : Form
     {
+        private Stack<Alumno> PilaAlumno = new Stack<Alumno>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +23,39 @@ namespace Crud2023
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("click realizado en boton guardar");
+            PilaAlumno.Push(new Alumno
+            { Carne = 464,
+              Celular = 646886,
+              PrimerNombre = "Cristiano",
+                SegundoNombre = "Lionel",
+                PrimerApellido ="Dos Santos",
+                SegundoApellido = "Messi",
+                TelefonoCasa = 16384768});
+            BindingSource bs = new BindingSource();
+            bs.DataSource = PilaAlumno;
+            dataGridView1.DataSource = bs;
+            dataGridView1.Refresh();
         }
+
+        public DataSet ObtenerAlumnos() {
+            try {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = @"Server=localhost\\SQLEXPRESS;Database=PrograIII2023COPY;Trusted_Connection=true";
+                    conn.Open();
+                    var select = "SELECT top 500 * FROM Alumno";
+                    var dataAdapter = new SqlDataAdapter(select, conn);
+                    var commandBuilder = new SqlCommandBuilder(dataAdapter);
+                    var ds = new DataSet();
+                    return ds;
+                 
+                }
+            } catch (Exception) {
+                throw;
+            }
+        }
+
 
         private void agregarAlumnoToolStripMenuItem_Click(object sender, EventArgs e)
         {
